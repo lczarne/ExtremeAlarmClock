d8752d18da0a77afc4fe80ebb563b0378cd71473
lukaszczarnecki <lczarne@gmail.com>
2012-11-10 15:32:05 +0100

    alarmViewController added

---
+++ ExtremeAlarmClock/ExtremeAlarmClock/AlarmViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlarmViewController.h
+//  ExtremeAlarmClock
+//
+//  Created by Łukasz Czarnecki on 11/10/12.
+//  Copyright (c) 2012 Łukasz Czarnecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlarmViewController : UIViewController
+
+@end
