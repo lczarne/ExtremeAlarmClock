3ffef7441761144e10037bc7d5fb9d7b4ade0b3d
miaka <miakaaa@gmail.com>
2012-11-10 17:45:27 +0100

    current time in alarm clock

--- ExtremeAlarmClock/ExtremeAlarmClock/XAlarmClockViewController.m
+++ ExtremeAlarmClock/ExtremeAlarmClock/XAlarmClockViewController.m
@@ -16,6 +16,7 @@
 
 - (IBAction)infoButtonPressed:(UIButton *)sender
 {
+    // set the alarm clock to the chosen number
 }
 
 
@@ -23,6 +24,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSDate *currentDateTime = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:@"HH"];
+    NSString *hour = [dateFormatter stringFromDate:currentDateTime];
+    [dateFormatter setDateFormat:@"mm"];
+    NSString *minute = [dateFormatter stringFromDate:currentDateTime];
+    self.hourCounterLabel.text = [NSString stringWithFormat:@"%@",hour];
+    self.minuteCounterLabel.text = [NSString stringWithFormat:@"%@",minute];
 }
 
 - (void)didReceiveMemoryWarning
